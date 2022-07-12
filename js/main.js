70ed0d30c6c76d613453eb996d5c48e136faa558
Kim Jueun <jrangkim11@gmail.com>
2022-07-12 17:53:54 +0900

    사진 넣기

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const bestPhoto = Array.from(document.getElementsByClassName('bestphoto'));
+console.log(bestPhoto);
+
+bestPhoto[0].style.backgroundImage = "url(/css/image/main/best/1.jpg)";
+bestPhoto[1].style.backgroundImage = "url(/css/image/main/best/2.jpg)";
+bestPhoto[2].style.backgroundImage = "url(/css/image/main/best/3.jpg)";
+bestPhoto[3].style.backgroundImage = "url(/css/image/main/best/4.jpg)";
